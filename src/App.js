bd46804944b074883d3adbbdb734e6761dc2872b
Shahnoor <64207572+Shahnoor-C@users.noreply.github.com>
2021-03-21 00:07:19 -0400

    first fetch pokemon API

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
 import logo from './logo.svg';
 import './App.css';
+import {useState} from 'react';
 
 function App() {
+  
+  const [allPokemon,setAllPokemon] = useState([])
+  
+  const getPokemon = ()=>{
+    console.log("clicked!")
+    // fetch("https://pokeapi.co/api/v2/pokemon")
+    //       .then(response => {
+    //         return response.json();
+    //     }).then(response => {
+    //         console.log(response);
+    //     }).catch(err=>{
+    //         console.log(err);
+     
+    fetch("https://pokeapi.co/api/v2/pokemon/?offset=20&limit=807")
+    .then(response =>{
+      console.log("*********")
+      console.log(response)
+      console.log("*********")
+      return response.json();
+    })
+    .then(response =>{
+      console.log("!!!!!!!!!!!!!!!!")
+      console.log(response)
+      console.log("!!!!!!!!!!!!!!!!")
+      setAllPokemon(response.results)
+    })
+    .catch(error =>{
+    
+      console.log(error)
+
+     
+    })
+
+  }
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick = {getPokemon}>Fetch Pokemon!</button>
+      {allPokemon.map((pokemon,idx)=>{
+        return <h1>{idx}: {pokemon.name}</h1>
+      })}
+     
+   
     </div>
   );
 }
